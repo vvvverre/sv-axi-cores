adf0ea6f8654c92cea00179d087a0bc9abf1e279
Wouter van Verre <woutervanverre@outlook.com>
2021-06-30 08:36:52 +0100

    Finish AXI BRAM interface
    Signed-off-by: Wouter van Verre <woutervanverre@outlook.com>

--- axi_bram_interface/rtl/axi_bram_interface.sv
+++ axi_bram_interface/rtl/axi_bram_interface.sv
@@ -6,7 +6,8 @@ module axi_bram_interface #
 (
     // Width of data bus in bits
     parameter AXI_DATA_WIDTH = 32,
-    parameter AXI_ADDR_WIDTH = 16
+    parameter AXI_ADDR_WIDTH = 12,
+    parameter BRAM_DATA_WIDTH = 24
 )
 (
     input  wire                             aclk,
@@ -16,6 +17,7 @@ module axi_bram_interface #
      * AXI4-Lite Slave Interface
      */
     input  wire [AXI_ADDR_WIDTH-1:0]        s_axil_araddr,
+    input  wire [2:0]                       s_axil_arprot,
     input  wire                             s_axil_arvalid,
     output reg                              s_axil_arready,
 
@@ -25,107 +27,85 @@ module axi_bram_interface #
     input  wire                             s_axil_rready,
 
     input  wire [AXI_ADDR_WIDTH-1:0]        s_axil_awaddr,
+    input  wire [2:0]                       s_axil_awprot,
     input  wire                             s_axil_awvalid,
     output reg                              s_axil_awready,
 
     input  wire [AXI_DATA_WIDTH-1:0]        s_axil_wdata,
     input  wire [AXI_DATA_WIDTH/8-1:0]      s_axil_wstrb,
     input  wire                             s_axil_wvalid,
-    output wire                             s_axil_wready,
+    output reg                              s_axil_wready,
 
-    output wire [1:0]                       s_axil_bresp,
-    output wire                             s_axil_bvalid,
+    output reg  [1:0]                       s_axil_bresp,
+    output reg                              s_axil_bvalid,
     input  wire                             s_axil_bready,
 
     /*
      * BRAM interface
      */
-    input  wire [DATA_WIDTH-1:0]     	    bram_rddata,
-    output reg  [DATA_WIDTH-1:0]     	    bram_wrdata,
-    output reg  [ADDR_WIDTH-1:0]		    bram_addr,
-    output reg  [(DATA_WIDTH)/8-1:0]   	    bram_we,
+    input  wire [BRAM_DATA_WIDTH-1:0]       bram_rddata,
+    output reg  [BRAM_DATA_WIDTH-1:0]       bram_wrdata,
+    output reg  [AXI_ADDR_WIDTH-2-1:0]	    bram_addr,
+    output reg  [(BRAM_DATA_WIDTH+7)/8-1:0] bram_we,
 	output reg  						    bram_en,
     output reg                              bram_clk
 );
 
-reg  [DATA_WIDTH-1:0]   rdata_buf;
-reg                     rdata_buf_valid;
+reg [AXI_DATA_WIDTH-1:0]        rdata_buf;
+reg                             rdata_buf_valid;
 
-wire                    write;
-wire                    bstall;
-wire                    rstall;
+wire                            bstall;
+wire                            rstall;
 
-assign write = s_axil_awvalid & s_axil_awready & s_axil_wvalid & s_axil_wready;
+wire                            read_eligible;
+wire                            write_eligible;
+
+wire [AXI_ADDR_WIDTH-2-1:0]     bram_wraddr;
+wire [AXI_ADDR_WIDTH-2-1:0]     bram_rdaddr;
+
 
 assign bstall = s_axil_bvalid && !s_axil_bready;
 assign rstall = s_axil_rvalid && !s_axil_rready;
 
+assign read_eligible  = s_axil_arvalid & !s_axil_arready & !rstall;
+assign write_eligible = s_axil_awvalid & !s_axil_awready & s_axil_wvalid & !s_axil_wready & !bstall;
 
-always_comb
-    if (!aresetn)
-        bram_addr <= 0;
-    else if (write)
-        bram_addr <= s_axil_awaddr;
-    else
-        bram_addr <= s_axil_araddr;
+assign bram_wraddr = s_axil_awaddr[AXI_ADDR_WIDTH-1:2];
+assign bram_rdaddr = s_axil_araddr[AXI_ADDR_WIDTH-1:2];
 
-always_ff @(posedge aclk)
-    if (!rdata_buf_valid)
+always_ff @(posedge aclk) begin
+    s_axil_arready <= aresetn & read_eligible;
+    s_axil_rvalid <= aresetn & (read_eligible | rstall);
+
+    s_axil_awready <= aresetn & write_eligible & !read_eligible;
+    s_axil_wready <= aresetn & write_eligible & !read_eligible;
+    s_axil_bvalid <= aresetn & ((write_eligible & !read_eligible) | bstall);
+
+    if (!rstall)
         rdata_buf <= bram_rddata;
+end
 
-always_ff @(posedge aclk)
-    if (s_axil_arvalid && s_axil_arready)
-        rdata_buf_valid <= 1'b1;
-    else if (s_axil_rvalid && s_axil_rready)
-        rdata_buf_valid <= 1'b0;
+always_comb begin
+    bram_wrdata = s_axil_wdata;
+    bram_en = write_eligible | read_eligible;
+    bram_clk = aclk;
 
-always_ff @(posedge aclk)
-    if (!aresetn)
-        s_axil_rvalid <= 1'b0;
-    else if ((s_axil_arvalid && s_axil_arready) || rstall)
-        s_axil_rvalid <= 1'b1;
+    if (write_eligible && !read_eligible) begin
+        bram_addr = bram_wraddr;
+        bram_we = s_axil_wstrb;
+    end else begin
+        bram_addr = bram_rdaddr;
+        bram_we = 0;
+    end
+
+    if (!rstall)
+        s_axil_rdata = bram_rddata;
     else
-        s_axil_rvalid <= 1'b0;
+        s_axil_rdata = rdata_buf;
 
-always_comb
-    if (rdata_buf_valid)
-        s_axil_rdata <= rdata_buf;
-    else
-        s_axil_rdata <= bram_rddata;
-
-always_ff @(posedge aclk)
-    s_axil_arready <= aresetn & s_axil_arvalid;
-
-always_ff @(posedge aclk)
-    s_axil_awready <= aresetn & s_axil_awvalid s_axil_wvalid & !s_axil_arvalid;
-    
-always_ff @(posedge aclk)
-    s_axil_wready <= aresetn & s_axil_awvalid s_axil_wvalid & !s_axil_arvalid;
-    
-always_ff @(posedge aclk)
-    if (write)
-        bram_wrdata <= s_axil_wdata;
-
-always_ff @(posedge aclk)
-    if (write)
-        bram_we <= s_axil_wstrb;
-    else
-        bram_we <= 0;
-
-always_ff @(posedge aclk)
-    bram_en <= 1'b1;
-
-always_ff @(posedge aclk)
-    s_axil_bvalid <= write | bstall;
-
-always_comb
-    s_axil_bresp <= 2'b00;
-
-always_comb
-    s_axil_rresp <= 2'b00;
-
-always_comb
-    bram_clk <= aclk;
+    s_axil_bresp = 2'b00;
+    s_axil_rresp = 2'b00;
+end
 
 endmodule
 
--- axi_bram_interface/test/cocotb/test_axi_bram_interface.py
+++ axi_bram_interface/test/cocotb/test_axi_bram_interface.py
@@ -1,6 +1,6 @@
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge, with_timeout
+from cocotb.triggers import RisingEdge, with_timeout, Timer
 from cocotb.regression import TestFactory
 
 from cocotbext.axi import AxiLiteBus, AxiLiteMaster
@@ -13,6 +13,7 @@ import logging
 import itertools
 import os.path
 import numpy as np
+import struct
 
 
 class TB:
@@ -67,7 +68,9 @@ def random_pause(f = 0.5):
 
 
 @cocotb.test()
-async def run_test(dut, nblocks=1, data_generator=None, idle_generator=None, backpressure_generator=None):
+async def run_test_read(dut, n_reads = 200, data_generator=None, idle_generator=None, backpressure_generator=None):
+    global rng
+
     tb = TB(dut)
     tb.set_idle_generator(idle_generator)
     tb.set_backpressure_generator(backpressure_generator)
@@ -77,27 +80,109 @@ async def run_test(dut, nblocks=1, data_generator=None, idle_generator=None, bac
     dut._log.info(f"param AXI_DATA_WIDTH = {dut.AXI_DATA_WIDTH.value}")
     dut._log.info(f"param AXI_ADDR_WIDTH = {dut.AXI_ADDR_WIDTH.value}")
 
-    frame_length = 2**(dut.ADDR_WIDTH.value)
-    frame_data = data_generator(frame_length)
-    contents = dict(zip(range(frame_length), map(int, frame_data)))
-    tb.bram.set_contents(contents)
+    bram_size = 2**(dut.AXI_ADDR_WIDTH.value-2)
+    bram_data = data_generator(bram_size)
+    tb.bram.set_contents(dict(zip(range(bram_size), map(int, bram_data))))
 
-    dut.limit <= 0
+    addrs = list(map(int, rng.choice(bram_size, size=n_reads, replace=False)))
+
     await tb.reset()
 
-    #recv_frame = await tb.sink.recv()
-    for nn in range(nblocks):
-        recv_frame = await tb.sink.recv()
-        
-        assert np.all(recv_frame.tdata == frame_data)
+    for addr in addrs:
+        response = await tb.axil_master.read(addr*4, 4)
+        assert int.from_bytes(response.data, 'little', signed=False) == bram_data[addr]
+
+    for _ in range(100):
+        await RisingEdge(dut.aclk)
+
+@cocotb.test()
+async def run_test_write(dut, n_writes = 200, data_generator=None, idle_generator=None, backpressure_generator=None):
+    global rng
+
+    tb = TB(dut)
+    tb.set_idle_generator(idle_generator)
+    tb.set_backpressure_generator(backpressure_generator)
+
+    data_generator = data_generator or (lambda x: np.full(x, 10))
+    
+    dut._log.info(f"param AXI_DATA_WIDTH = {dut.AXI_DATA_WIDTH.value}")
+    dut._log.info(f"param AXI_ADDR_WIDTH = {dut.AXI_ADDR_WIDTH.value}")
+
+    bram_size = 2**(dut.AXI_ADDR_WIDTH.value-2)
+    bram_data = data_generator(bram_size)
+    addrs = list(map(int, rng.choice(bram_size, size=n_writes, replace=False)))
+    contents = dict()
+
+    await tb.reset()
+
+    for addr in addrs:
+        await tb.axil_master.write(addr*4, struct.pack("<I", bram_data[addr]))
+        contents[addr] = bram_data[addr]
+
+    tb.bram.verify(contents)
+
+    for _ in range(100):
+        await RisingEdge(dut.aclk)
+
+@cocotb.test()
+async def run_test_mixed(dut, nworkers=16):
+    global rng
+
+    tb = TB(dut)
+    
+    dut._log.info(f"param AXI_DATA_WIDTH = {dut.AXI_DATA_WIDTH.value}")
+    dut._log.info(f"param AXI_ADDR_WIDTH = {dut.AXI_ADDR_WIDTH.value}")
+
+    bram_size = 2**(dut.AXI_ADDR_WIDTH.value-2)
+
+    async def worker(master, offset, aperture, seed, count=16):
+        worker_rng = np.random.default_rng(seed)
+        lengths = worker_rng.integers(1, min(32, aperture), count, endpoint=True)
+        for length in lengths:
+            data = worker_rng.integers(0, 256, length)
+            test_data = bytearray(map(int, data))
+            addr = int(offset + worker_rng.integers(0, aperture-length, 1)[0])
+
+            delay = int(worker_rng.integers(1, 100, 1)[0])
+            await Timer(delay, 'ns')
+
+            await master.write(addr, test_data)
+
+            delay = int(worker_rng.integers(1, 100, 1)[0])
+            await Timer(delay, 'ns')
+
+            response = await master.read(addr, length)
+
+            assert response.data == test_data
+
+    aperture = bram_size // nworkers
+    offsets = np.arange(nworkers) * aperture
+    seeds = rng.integers(2*32, size=nworkers)
+
+    workers = list()
+    for offset, seed in zip(offsets, seeds):
+        workers.append(cocotb.fork(worker(tb.axil_master, offset, aperture, seed, count=32)))
+
+    while workers:
+        await workers.pop(0).join()
 
     for _ in range(100):
         await RisingEdge(dut.aclk)
 
 
 if cocotb.SIM_NAME:
-    factory = TestFactory(run_test)
+    factory = TestFactory(run_test_read)
+    factory.add_option("data_generator", [block_data_linear, block_data_random])
+    factory.add_option("idle_generator", [None, cycle_pause, random_pause])
+    factory.add_option("backpressure_generator", [None, cycle_pause, random_pause])
     factory.generate_tests()
+
+    factory = TestFactory(run_test_write)
+    factory.add_option("data_generator", [block_data_linear, block_data_random])
+    factory.add_option("idle_generator", [None, cycle_pause, random_pause])
+    factory.add_option("backpressure_generator", [None, cycle_pause, random_pause])
+    factory.generate_tests()
+
 
 rng = np.random.default_rng(12345)
 
@@ -107,9 +192,8 @@ rtl_dir = os.path.abspath(os.path.join(tests_dir, '..', '..', 'rtl'))
 root_dir = os.path.abspath(os.path.join(tests_dir, '..', '..', '..'))
 
 
-@pytest.mark.parametrize("axi_data_width", [16, 32])
-@pytest.mark.parametrize("axi_addr_width", [8, 12])
-def test_axis_bram_reader(request, axi_data_width, axi_addr_width):
+@pytest.mark.parametrize("axi_addr_width", [12, 16])
+def test_axis_bram_interface(request, axi_addr_width):
     dut = "axi_bram_interface"
     module = os.path.splitext(os.path.basename(__file__))[0]
     toplevel = dut
@@ -119,7 +203,6 @@ def test_axis_bram_reader(request, axi_data_width, axi_addr_width):
     ]
 
     parameters = dict()
-    parameters["AXI_DATA_WIDTH"] = axi_data_width
     parameters["AXI_ADDR_WIDTH"] = axi_addr_width
 
     extra_env = {f'PARAM_{k}': str(v) for k, v in parameters.items()}
